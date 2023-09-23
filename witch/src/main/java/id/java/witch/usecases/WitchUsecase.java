968c8eb53e2999fd14234733b152e21f590ef2c7
Ida Milati Sufiana <idcb.idam@xl.co.id>
2023-09-23 16:32:19 +0700

    fix prima

--- witch/src/main/java/id/java/witch/usecases/WitchUsecase.java
+++ witch/src/main/java/id/java/witch/usecases/WitchUsecase.java
@@ -16,9 +16,9 @@ public class WitchUsecase{
         prima.put(1, 1);
         int urut = 0;
 
-        for (int i=1; i<=100; i++)
+        for (int i=2; i<=100; i++)
         {
-                int Check=0;
+            int Check=0;
             for (int j=1; j<=i; j++)
             {
                 if (i%j==0)
